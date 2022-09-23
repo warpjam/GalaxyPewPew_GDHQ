a2cad12c756124ca4ec30a2ff600e6655d70c40a
warpjam <warpjam@gmail.com>
2022-09-23 17:54:44 -0400

    Transfered from CRLF issue

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3.5f;
+
+
+    void Start()
+    {
+
+        transform.position = new Vector3(0, 0, 0);
+
+    }
+    
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        
+        transform.Translate(direction * _speed * Time.deltaTime);
+        
+
+        if (transform.position.y >= 5.79f)
+        {
+            transform.position = new Vector3(transform.position.x, 5.79f, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        if (transform.position.x > 11f)
+        {
+            transform.position = new Vector3(-11, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11f)
+        {
+            transform.position = new Vector3(11, transform.position.y, 0);
+        }
+    }
+}
